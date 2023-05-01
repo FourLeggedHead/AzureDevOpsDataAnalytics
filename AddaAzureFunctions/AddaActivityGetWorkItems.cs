936a0c69b2aee8f429b0ae21b503fb85a9def0f7
Eric Lauret <erlau@pandora.net>
2023-05-01 09:39:45 +0200

    Issue GH-8: Added code to query parents, grand parents and great grand parents; refactored to inject http clients to support test automation

--- AddaAzureFunctions/AddaActivityGetWorkItems.cs
+++ AddaAzureFunctions/AddaActivityGetWorkItems.cs
@@ -30,20 +30,17 @@ namespace ADDA.Functions
             "System.WorkItemType",
             "System.Parent",
             "Microsoft.VSTS.Common.ClosedDate",
-            "Microsoft.VSTS.Scheduling.CompletedWork",
-            "GlobalBI.Typeorprojectnr",
-            "Custom.Capitalizable",
-            "Custom.Category"
+            "Microsoft.VSTS.Scheduling.CompletedWork"
         };
 
         [FunctionName(nameof(GetAzdoWorkItems))]
         [StorageAccount("DevOpsDataStorageAppSetting")]
         public static async Task GetAzdoWorkItems([ActivityTrigger] IAddaDevOpsOrganization organization,
                             [Table("DevOpsProjects")] TableClient tableClient,
-                            [Blob("bronze/tasks_{sys.utcnow}.json", FileAccess.Write)] Stream tasksData,
-                            [Blob("bronze/pbis_{sys.utcnow}.json", FileAccess.Write)] Stream pbisData,
-                            [Blob("bronze/features_{sys.utcnow}.json", FileAccess.Write)] Stream featuresData,
-                            [Blob("bronze/epics_{sys.utcnow}.json", FileAccess.Write)] Stream epicsData, ILogger log)
+                            [Blob("bronze/tasks/tasks_{sys.utcnow}.json", FileAccess.Write)] Stream tasksData,
+                            [Blob("bronze/pbis/pbis_{sys.utcnow}.json", FileAccess.Write)] Stream pbisData,
+                            [Blob("bronze/features/features_{sys.utcnow}.json", FileAccess.Write)] Stream featuresData,
+                            [Blob("bronze/epics/epics_{sys.utcnow}.json", FileAccess.Write)] Stream epicsData, ILogger log)
         {
             log.LogInformation($"Getting the list of work items for project collection {organization.OrganizationUri}.");
 
@@ -61,39 +58,55 @@ namespace ADDA.Functions
 
             log.LogInformation($"Selected projects: {string.Join(", ", selectedProjects)}.");
 
-            organization.GetCredential();
+            var credential = organization.GetCredential();
+            using var workClient = new WorkHttpClient(organization.OrganizationUri, credential);
+            using var workItemsClient = new WorkItemTrackingHttpClient(organization.OrganizationUri, credential);
 
             // Get done tasks from selected projects
             var iterationPath = "2023";
 
             log.LogInformation($"Fetch done tasks in selected projects, under iteration path {iterationPath}.");
-            var doneTasks = await GetDoneTasks(organization, selectedProjects, iterationPath, log);
+            var doneTasks = await GetDoneTasks(workClient, workItemsClient, organization, selectedProjects, iterationPath, log);
             log.LogInformation($"Listed {doneTasks.Count()} done Tasks.");
             await JsonSerializer.SerializeAsync(tasksData, doneTasks);
             log.LogInformation("Stored done tasks in Azure as json.");
+
+            log.LogInformation($"Fetch parent PBIs in selected projects, under iteration path {iterationPath}.");
+            var parentPbis = await GetParentWorkItems(workItemsClient, iterationPath, doneTasks).ConfigureAwait(false);
+            log.LogInformation($"Listed {parentPbis.Count()} parent PBIs.");
+            await JsonSerializer.SerializeAsync(pbisData, parentPbis);
+            log.LogInformation("Stored parent PBIs in Azure as json.");
+
+            log.LogInformation($"Fetch parent Features in selected projects, under iteration path {iterationPath}.");
+            var parentFeatures = await GetParentWorkItems(workItemsClient, iterationPath, parentPbis).ConfigureAwait(false);
+            log.LogInformation($"Listed {parentFeatures.Count()} parent Features.");
+            await JsonSerializer.SerializeAsync(featuresData, parentFeatures);
+            log.LogInformation("Stored parent Features in Azure as json.");
+
+            log.LogInformation($"Fetch parent Epics in selected projects, under iteration path {iterationPath}.");
+            var parentEpics = await GetParentWorkItems(workItemsClient, iterationPath, parentFeatures).ConfigureAwait(false);
+            log.LogInformation($"Listed {parentEpics.Count()} parent Epics.");
+            await JsonSerializer.SerializeAsync(epicsData, parentEpics);
+            log.LogInformation("Stored parent Epics in Azure as json.");
         }
 
         // Verify that the project has the specified iteration path
-        public static bool ProjectHasIterationPath(IAddaDevOpsOrganization organization, string project, string iterationPath)
+        public static async Task<bool> ProjectHasIterationPath(WorkHttpClient workClient, string project, string iterationPath)
         {
             try
             {
-                var credential = organization.GetCredential();
-
-                using (var workClient = new WorkHttpClient(organization.OrganizationUri, credential))
-                {
-                    var iterations = workClient.GetTeamIterationsAsync(new TeamContext(project)).Result;
-                    return iterations.Any(i => i.Path.Contains(String.Concat(project, "\\", iterationPath)));
-                }
+                var iterations = await workClient.GetTeamIterationsAsync(new TeamContext(project));
+                return iterations.Any(i => i.Path.Contains(String.Concat(project, "\\", iterationPath)));
             }
-            catch (System.Exception ex)
+            catch (Exception ex)
             {
                 throw new Exception($"Could not get iterations for project {project}", ex);
             }
         }
 
         // Query all the tasks in state Done that belong to the selected project and are under the specified iteration path
-        public static async Task<IEnumerable<WorkItem>> GetDoneTasks(IAddaDevOpsOrganization organization, IEnumerable<string> projects, string iterationPath, ILogger log)
+        public static async Task<IEnumerable<WorkItem>> GetDoneTasks(WorkHttpClient workClient, WorkItemTrackingHttpClient workItemsClient,
+                                        IAddaDevOpsOrganization organization, IEnumerable<string> projects, string iterationPath, ILogger log)
         {
             var workItemIds = new List<int>();
 
@@ -101,7 +114,7 @@ namespace ADDA.Functions
             {
                 try
                 {
-                    if (ProjectHasIterationPath(organization, project, iterationPath) == false)
+                    if (await ProjectHasIterationPath(workClient, project, iterationPath) == false)
                     {
                         log.LogInformation($"Project {project} does not have iteration path {project}\\{iterationPath}.");
                         continue;
@@ -112,12 +125,12 @@ namespace ADDA.Functions
                         Query = "Select [Id] " +
                                     "From WorkItems " +
                                     "Where [Work Item Type] = 'Task' " +
-                                    "And [State] = 'Done' " +
+                                    "And [State] In ('Closed', 'Done') " +
                                     $"And [Iteration Path] Under '{project}\\{iterationPath}' " +
                                     $"And [Team Project] = '{project}'"
                     };
 
-                    workItemIds.AddRange(await QueryWorkItemsIds(organization, wiql));
+                    workItemIds.AddRange(await QueryWorkItemsIds(workItemsClient, wiql));
                 }
                 catch (System.Exception ex)
                 {
@@ -127,7 +140,7 @@ namespace ADDA.Functions
 
             try
             {
-                return await GetWorkItemsWithIdsAsync(organization, workItemIds).ConfigureAwait(false);
+                return await GetWorkItemsWithIdsAsync(workItemsClient, workItemIds).ConfigureAwait(false);
             }
             catch (System.Exception ex)
             {
@@ -136,17 +149,12 @@ namespace ADDA.Functions
         }
 
         /// Query the work item ids based on a Work Item query
-        public static async Task<List<int>> QueryWorkItemsIds(IAddaDevOpsOrganization organization, Wiql query)
+        public static async Task<List<int>> QueryWorkItemsIds(WorkItemTrackingHttpClient workItemsClient, Wiql query)
         {
             try
             {
-                var credential = organization.GetCredential();
-
-                using (var workItemsClient = new WorkItemTrackingHttpClient(organization.OrganizationUri, credential))
-                {
-                    var result = await workItemsClient.QueryByWiqlAsync(query);
-                    return result.WorkItems.Select(item => item.Id).ToList();
-                }
+                var result = await workItemsClient.QueryByWiqlAsync(query);
+                return result.WorkItems.Select(item => item.Id).ToList();
             }
             catch (VssException ex)
             {
@@ -157,21 +165,31 @@ namespace ADDA.Functions
         /// Get work items and chosen fields based on ids and a list of fields to retrieve
         /// Will slice the ids into batches of 200 to pass to the api
         /// The methods takes care of prefixing the fields with"System." 
-        public static async Task<IList<WorkItem>> GetWorkItemsWithIdsAsync(IAddaDevOpsOrganization organization, ICollection<int> ids)
+        public static async Task<IList<WorkItem>> GetWorkItemsWithIdsAsync(WorkItemTrackingHttpClient workItemsClient, ICollection<int> ids)
         {
             var windowedIds = ids.Batch(200);
             var workItemList = new List<WorkItem>();
-            var credential = organization.GetCredential();
 
-            using (var workItemsClient = new WorkItemTrackingHttpClient(organization.OrganizationUri, credential))
+            foreach (var idsWindow in windowedIds)
             {
-                foreach (var idsWindow in windowedIds)
-                {
-                    workItemList.AddRange(await workItemsClient.GetWorkItemsAsync(idsWindow, WorkItemFields).ConfigureAwait(false));
-                }
+                workItemList.AddRange(await workItemsClient.GetWorkItemsAsync(idsWindow, WorkItemFields).ConfigureAwait(false));
             }
 
             return workItemList;
+        }
+
+        // Query the parent work items for the list of work items provided
+        public static async Task<IEnumerable<WorkItem>> GetParentWorkItems(WorkItemTrackingHttpClient workItemsClient, string iterationPath, IEnumerable<WorkItem> workItems)
+        {
+            try
+            {
+                var workItemIds = workItems.Select(w => Int32.Parse(w.Fields["System.Parent"].ToString())).Distinct().ToList();
+                return await GetWorkItemsWithIdsAsync(workItemsClient, (ICollection<int>)workItemIds).ConfigureAwait(false);
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception($"Could not get parent work items under itration path {iterationPath}", ex);
+            }
         }
     }
 }
