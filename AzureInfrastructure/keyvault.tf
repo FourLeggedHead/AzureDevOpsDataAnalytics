3e984c4329831826a0614384c07bd8da565574f1
Eric Lauret <erlau@pandora.net>
2023-07-13 12:24:33 +0200

    Issue GH-7 - Added Access Policy for the function app to be able to read secrets

--- AzureInfrastructure/keyvault.tf
+++ AzureInfrastructure/keyvault.tf
@@ -28,3 +28,25 @@ resource "azurerm_key_vault" "bactool_kv" {
     ]
   }
 }
+
+# Creates a secret to hold AZDO access token
+resource "azurerm_key_vault_secret" "bactool_azdo_secret" {
+  name         = "AzureDevOpsAccessToken"
+  value        = "ToBeDefined"
+  key_vault_id = azurerm_key_vault.bactool_kv.id
+}
+
+# Creates access policy for the function app to read secrets
+resource "azurerm_key_vault_access_policy" "bactool_function_app_ap" {
+  key_vault_id = azurerm_key_vault.bactool_kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_function_app.bactool_func_app.identity[0].principal_id
+
+  key_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get",
+  ]
+}
